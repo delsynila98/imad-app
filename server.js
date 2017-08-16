f97af8e1d740818f590a1176b6f855e1c437ae50
delsynila98 <30645613+delsynila98@users.noreply.github.com>
2017-08-16 11:30:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('article-one',function (req,res){
-  res_send('article-one requested and wll be served here');    
+  res_send('article-one requested and will be served here');    
 });
 
 app.get('article-two',function (req,res){
-  res_send('article-two requested and wll be served here');    
+  res_send('article-two requested and will be served here');    
 });
 
 app.get('article-three',function (req,res){
-  res_send('article-three requested and wll be served here');    
+  res_send('article-three requested and will be served here');    
 });
 
 app.get('/ui/style.css', function (req, res) {
